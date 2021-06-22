7b047de9a43b4a7f99a331bfe4f4099baa73ea25
Parth Pant <parthpant4@gmail.com>
2021-06-22 22:12:30 +0530

    git gutter and hunk keybinding

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
  ;; If there is more than one, they won't work right.
  '(org-agenda-files '("~/Org/agenda.org"))
  '(package-selected-packages
-   '(treemacs git-gutter+ git-gutter org-mode visual-fill-column company c-mode c++-mode lsp-clients lsp-ui lsp-mode hydra all-the-icons-ivy-rich evil-magit counsel-projectile ivy-rich counsel neotree dashboard projectile gcmh doom-modeline toc-org org-bullets evil-tutor evil-collection evil use-package))
+   '(git-gutter-fringe treemacs git-gutter+ git-gutter org-mode visual-fill-column company c-mode c++-mode lsp-clients lsp-ui lsp-mode hydra all-the-icons-ivy-rich evil-magit counsel-projectile ivy-rich counsel neotree dashboard projectile gcmh doom-modeline toc-org org-bullets evil-tutor evil-collection evil use-package))
  '(safe-local-variable-values
    '((c-file-offsets
       (innamespace . 0)
