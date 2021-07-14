b7451a31d0b37818ee73ac3af528c30e87b40cec
Parth Pant <parthpant4@gmail.com>
2021-07-14 21:25:14 +0530

    minor improvements

--- init.el
+++ init.el
@@ -8,7 +8,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil dired which-key vterm visual-fill-column use-package treemacs toc-org peep-dired org-bullets no-littering neotree magit lsp-ui lsp-ivy git-gutter-fringe git-gutter+ general gcmh evil-tutor evil-numbers evil-collection eterm-256color doom-themes doom-modeline dired-open dashboard counsel-projectile company-box all-the-icons-ivy-rich all-the-icons-dired)))
+   '(lsp-treemacs flycheck treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil dired which-key vterm visual-fill-column use-package treemacs toc-org peep-dired org-bullets no-littering neotree magit lsp-ui lsp-ivy git-gutter-fringe git-gutter+ general gcmh evil-tutor evil-numbers evil-collection eterm-256color doom-themes doom-modeline dired-open dashboard counsel-projectile company-box all-the-icons-ivy-rich all-the-icons-dired))
+ '(safe-local-variable-values
+   '((c-file-offsets
+      (innamespace . 0)
+      (inline-open . 0)
+      (case-label . +)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
